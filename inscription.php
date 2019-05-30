2257e85d17fff70ac840b2ce7315293e11c695b5
Hocine Abdellatif Houari <houarihocine99@gmail.com>
2019-05-30 05:58:17 +0100

    added handling inscriptions in inscription.php improved the ui/ux in inscription-étudiant.html added a front picture in index.html

--- inscription.php
+++ inscription.php
@@ -1,9 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Vérification</title>
+  <style>
+    @font-face {
+      font-family: "cairo";
+      src: url('fonts/Cairo-SemiBold.ttf')
+    }
+
+    * {
+      font-family: "cairo";
+    }
+
+    body {
+      margin: 0;
+    }
+
+    body>div:first-child {
+      text-align: center;
+      height: 100vh;
+      padding: 50px;
+    }
+
+    div.return {
+      margin-top: 50px;
+    }
+
+    div#note {
+      text-align: left;
+      max-width: 500px;
+      min-width: 400px;
+      margin: auto;
+      margin-top: 26px;
+      border-left: 5px #ff5516 solid;
+      padding: 20px 10px;
+      background-color: #edf7c4;
+    }
+
+    a {
+      text-decoration: none;
+      color: black;
+      padding: 2px 10px;
+      margin: 36px 0 0;
+      border-radius: 6px;
+      border: 1px solid #0006;
+      background-color: #f6f6f6;
+    }
+
+    a:hover {
+      background-color: #e6e6e6;
+    }
+  </style>
+</head>
+
+<body>
+  <?php
 $dbhost = "localhost";
 $dbuser = "hocine";
 $dbpass = "hocine";
 $dbname = "daw";
-
+// connecting to databases
 $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname, );
 if ($connection->connect_error) die("Could not connect to the database.");
 
@@ -12,23 +72,8 @@ unset($dbuser);
 unset($dbpass);
 unset($dbname);
 
+$success = false;
 
-// echo '$_POST[\'nom\']: ' . $_POST['nom'] . '<br>';
-// echo '$_POST[\'prenom\']: ' . $_POST['prenom'] . '<br>';
-// echo '$_POST[\'daten\']: ' . $_POST['daten'] . '<br>';
-// echo '$_POST[\'lieun\']: ' . $_POST['lieun'] . '<br>';
-// echo '$_POST[\'email\']: ' . $_POST['email'] . '<br>';
-// echo '$_POST[\'pass\']: ' . $_POST['pass'] . '<br>';
-// echo '$_POST[\'tel\']: ' . $_POST['tel'] . '<br>';
-// echo '$_POST[\'nationalite\']: ' . $_POST['nationalite'] . '<br>';
-// echo '$_POST[\'pays\']: ' . $_POST['pays'] . '<br>';
-// echo '$_POST[\'adresse\']: ' . $_POST['adresse'] . '<br>';
-// echo '$_POST[\'lang\']: ' . $_POST['lang'] . '<br>';
-// echo '$_POST[\'niveau\']: ' . $_POST['niveau'] . '<br>';
-// echo '$_POST[\'diplome\']: ' . $_POST['diplome'] . '<br>';
-// echo '$_POST[\'exeperionce\']: ' . $_POST['exeperionce'] . '<br>';
-// echo '$_POST[\'dated\']: ' . $_POST['dated'] . '<br>';
-// echo '$_POST[\'anneexeperionce\']: ' . $_POST['anneexeperionce'] . '<br>';
 
 if (isset($_POST['nom']) && isset($_POST['prenom']) &&
     isset($_POST['daten']) && isset($_POST['lieun']) &&
@@ -36,13 +81,78 @@ if (isset($_POST['nom']) && isset($_POST['prenom']) &&
     isset($_POST['tel']) && isset($_POST['nationalite']) &&
     isset($_POST['pays']) && isset($_POST['adresse']) &&
     isset($_POST['lang']) && isset($_POST['niveau']) &&
-    isset($_POST['diplome']) && isset($_POST['dated']) &&
-    isset($_POST['exeperionce']) && isset($_POST['anneexeperionce'])
-
+    isset($_POST['diplome']) && isset($_POST['dated'])
   ) {
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $email = $_POST['email'];
-    $result = $connection->query("INSERT INTO etudiants VALUES(NULL, '$nom', '$prenom', '$email')");
-    if (!$result) die ("Could not add user: $nom $prenom");
+    $tel = $_POST['tel'];
+    // les champs de experience et anneexperience ne sont pas obliguatoires.
+    if (isset($_POST['experience']) && isset($_POST['anneexperience'])) {
+      // on utilisée plus tard.
+      $experience = $_POST['experience'];
+      $anneexperience = $_POST['anneexperience'];
+    }
+    // check email and phone number if already exist in our database: stop registration, else continue.
+    $result = $connection->query("SELECT * FROM etudiants WHERE etd_email='$email' OR etd_tel='$tel'");
+    if (!$result->num_rows) {
+      // insert new user.
+      $result = $connection->query("INSERT INTO etudiants VALUES(NULL, '$nom', '$prenom', '$email', '$tel')");
+      if ($result) {
+        // inserting new user successfully.
+        $success = true;
+        $successmsg = <<< _SUCCESS_MSG
+        <div>Félicitations $nom! Votre compte a été créé.</div>
+        <div id="note">
+          <span style="background-color: #ff5516;">&nbsp;Note:&nbsp;</span>
+          &nbsp;Cet compte va être supprimer automatiquement si vous avez été réfusés
+          ou après la réalisation de concours.
+        </div>
+_SUCCESS_MSG;
+      } else {
+        // inserting new user failed.
+        $success = false;
+        $failmsg = <<< _FAIL_MSG
+          <div>Désolé, on a un problème unconnue pour le moment, reéssayer après un moment.</div>
+_FAIL_MSG;
+      }
+    } else {
+      // email/tel already exists on the database.
+      $success = false;
+      $failmsg = <<< _FAIL_MSG
+        <div>Désolé, ce e-mail ou/et téléphone a déjà utilisé.</div>
+_FAIL_MSG;
+    }
+  } else {
+    // didn't enter all required informations.
+    $success = false;
+    $failmsg = <<< _FAIL_MSG
+      <div>Désolé, vous devez remplir tous les informations nécessaire pour une inscription .</div>
+_FAIL_MSG;
   }
+
+if ($success) {
+  echo <<< _BEGIN
+    <div style="background-color: #deffe0;">
+      <div><img src="img/success.png" alt="Success" style="width: 180px;"></div>
+_BEGIN;
+  echo $successmsg;
+  echo <<< _END
+      <div class="return"><a href=".">returnez à la page principale</a></div>
+    </div>
+_END;
+} else {
+  echo <<< _BEGIN
+    <div style="background-color: #fff5f1;">
+      <div><img src="img/fail.png" alt="Failed" style="width: 180px;"></div>
+_BEGIN;
+  echo $failmsg;
+  echo <<< _END
+      <div class="return"><a href="inscription-etudiant.html">returnez à la page d'inscription</a></div>
+    </div>
+_END;
+}
+?>
+</body>
+
+</html>
